8a2ec9cbc7f78be2085ba005158e4472154cfe42
sistemanixa <nixa@sistemanixa.com>
2026-07-24 01:45:26 -0300

    Corregir versiones del aviso de actualizacion

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 ﻿/* SisVentas NIXA - Service Worker v2.0.183
-   Versión v2.0.188. Estrategia: red primero con cache de respaldo. */
-const CACHE = 'sisventas-v2.0.188';
+   Versión v2.0.189. Estrategia: red primero con cache de respaldo. */
+const CACHE = 'sisventas-v2.0.189';
 const SHELL = [
   './',
   './index.html',
   './css/app.css',
   './js/app.js',
-  './js/app.v2.0.188.js',
+  './js/app.v2.0.189.js',
   './js/core/version.js',
-  './js/core/version.v2.0.188.js',
+  './js/core/version.v2.0.189.js',
   './js/core/login.js',
   './js/core/access-control.js',
   './js/core/firebase.js',
