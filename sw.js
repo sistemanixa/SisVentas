3bc63c35dfac343406efdc80097096525c9a5e31
sistemanixa <nixa@sistemanixa.com>
2026-07-24 02:03:37 -0300

    Permitir sesion persistente para admin

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 ﻿/* SisVentas NIXA - Service Worker v2.0.183
-   Versión v2.0.189. Estrategia: red primero con cache de respaldo. */
-const CACHE = 'sisventas-v2.0.189';
+   Versión v2.0.190. Estrategia: red primero con cache de respaldo. */
+const CACHE = 'sisventas-v2.0.190';
 const SHELL = [
   './',
   './index.html',
   './css/app.css',
   './js/app.js',
-  './js/app.v2.0.189.js',
+  './js/app.v2.0.190.js',
   './js/core/version.js',
-  './js/core/version.v2.0.189.js',
+  './js/core/version.v2.0.190.js',
   './js/core/login.js',
   './js/core/access-control.js',
   './js/core/firebase.js',
