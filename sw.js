a3f3ebba1111d2300c5498e0ac13784cfe2e6dc7
sistemanixa <nixa@sistemanixa.com>
2026-07-27 16:52:53 -0300

    Agregar duplicación y cálculo de productos por metro

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-/* SisVentas NIXA - Service Worker v2.0.200
-   Versión v2.0.200. Estrategia: red primero con cache de respaldo. */
-const CACHE = 'sisventas-v2.0.200';
+/* SisVentas NIXA - Service Worker v2.0.201
+   Versión v2.0.201. Estrategia: red primero con cache de respaldo. */
+const CACHE = 'sisventas-v2.0.201';
 const SHELL = [
   './',
   './index.html',
   './css/app.css',
   './js/app.js',
-  './js/app.v2.0.200.js',
+  './js/app.v2.0.201.js',
   './js/core/version.js',
-  './js/core/version.v2.0.200.js',
+  './js/core/version.v2.0.201.js',
   './js/core/login.js',
   './js/core/access-control.js',
   './js/core/firebase.js',
