3805004ee3f54cdb1265bdd8f3688ce44b6430be
sistemanixa <nixa@sistemanixa.com>
2026-08-03 15:26:58 -0300

    Publicar v2.0.277 con gastos mensuales claros

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-/* SisVentas NIXA - Service Worker v2.0.276
-   Versión v2.0.276. Estrategia: red primero con cache de respaldo. */
-const CACHE = 'sisventas-v2.0.276';
+/* SisVentas NIXA - Service Worker v2.0.277
+   Versión v2.0.277. Estrategia: red primero con cache de respaldo. */
+const CACHE = 'sisventas-v2.0.277';
 const SHELL = [
   './',
   './index.html',
   './css/app.css',
   './js/app.js',
-  './js/app.v2.0.276.js',
+  './js/app.v2.0.277.js',
   './js/core/version.js',
-  './js/core/version.v2.0.276.js',
+  './js/core/version.v2.0.277.js',
   './js/core/login.js',
   './js/core/access-control.js',
   './js/core/firebase.js',
