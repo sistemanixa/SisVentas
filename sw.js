c14d2fc79a3c058c63d8e4435808ad16cc79a113
sistemanixa <nixa@sistemanixa.com>
2026-08-02 23:36:34 -0300

    Adaptar cargos a tarjetas responsive

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-/* SisVentas NIXA - Service Worker v2.0.252
-   Versión v2.0.252. Estrategia: red primero con cache de respaldo. */
-const CACHE = 'sisventas-v2.0.252';
+/* SisVentas NIXA - Service Worker v2.0.253
+   Versión v2.0.253. Estrategia: red primero con cache de respaldo. */
+const CACHE = 'sisventas-v2.0.253';
 const SHELL = [
   './',
   './index.html',
   './css/app.css',
   './js/app.js',
-  './js/app.v2.0.252.js',
+  './js/app.v2.0.253.js',
   './js/core/version.js',
-  './js/core/version.v2.0.252.js',
+  './js/core/version.v2.0.253.js',
   './js/core/login.js',
   './js/core/access-control.js',
   './js/core/firebase.js',
