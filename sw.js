e53bc3fe28a25d05e33ac36234ba5cdbd801185c
sistemanixa <nixa@sistemanixa.com>
2026-08-02 22:45:26 -0300

    Agregar copia de valor hora a horas extra v2.0.250

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-/* SisVentas NIXA - Service Worker v2.0.249
-   Versión v2.0.249. Estrategia: red primero con cache de respaldo. */
-const CACHE = 'sisventas-v2.0.249';
+/* SisVentas NIXA - Service Worker v2.0.250
+   Versión v2.0.250. Estrategia: red primero con cache de respaldo. */
+const CACHE = 'sisventas-v2.0.250';
 const SHELL = [
   './',
   './index.html',
   './css/app.css',
   './js/app.js',
-  './js/app.v2.0.249.js',
+  './js/app.v2.0.250.js',
   './js/core/version.js',
-  './js/core/version.v2.0.249.js',
+  './js/core/version.v2.0.250.js',
   './js/core/login.js',
   './js/core/access-control.js',
   './js/core/firebase.js',
