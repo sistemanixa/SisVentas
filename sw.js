d4bb1860d2fa35acde94ad11455c60d0b191f173
sistemanixa <nixa@sistemanixa.com>
2026-08-04 10:35:20 -0300

    Ejecutar actualizador en segundo plano v2.0.284

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-/* SisVentas NIXA - Service Worker v2.0.283
-   Versión v2.0.283. Estrategia: red primero con cache de respaldo. */
-const CACHE = 'sisventas-v2.0.283';
+/* SisVentas NIXA - Service Worker v2.0.284
+   Versión v2.0.284. Estrategia: red primero con cache de respaldo. */
+const CACHE = 'sisventas-v2.0.284';
 const SHELL = [
   './',
   './index.html',
   './css/app.css',
   './js/app.js',
-  './js/app.v2.0.283.js',
+  './js/app.v2.0.284.js',
   './js/core/version.js',
-  './js/core/version.v2.0.283.js',
+  './js/core/version.v2.0.284.js',
   './js/core/login.js',
   './js/core/access-control.js',
   './js/core/firebase.js',
