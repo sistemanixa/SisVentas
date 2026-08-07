e1a71e033d10fa21b46a51cbfc114f5614918c2e
sistemanixa <nixa@sistemanixa.com>
2026-08-07 00:29:24 -0300

    Publicar correcciones de identidad como v2.0.290

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-/* SisVentas NIXA - Service Worker v2.0.289
-   Versión v2.0.289. Estrategia: red primero con cache de respaldo. */
-const CACHE = 'sisventas-v2.0.289';
+/* SisVentas NIXA - Service Worker v2.0.290
+   Versión v2.0.290. Estrategia: red primero con cache de respaldo. */
+const CACHE = 'sisventas-v2.0.290';
 const SHELL = [
   './',
   './index.html',
   './css/app.css',
   './js/app.js',
-  './js/app.v2.0.289.js',
+  './js/app.v2.0.290.js',
   './js/core/version.js',
-  './js/core/version.v2.0.289.js',
+  './js/core/version.v2.0.290.js',
   './js/core/login.js',
   './js/core/access-control.js',
   './js/core/firebase.js',
